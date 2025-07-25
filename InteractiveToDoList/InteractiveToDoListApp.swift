c2cc0c3b82e51343d5d3e915d0e349d61b188ccf
kwk Cecilia .C <cc.comar36@gmail.com>
2025-07-25 13:03:45 -0400

    Initial Commit

---
+++ InteractiveToDoList/InteractiveToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveToDoListApp.swift
+//  InteractiveToDoList
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
